bd7f9cfa7eacb0026b00655664c2ddd2eb654ca2
Hebert Saravalle <79430844+HebertVS@users.noreply.github.com>
2023-06-05 12:59:55 -0300

    Atualização do Projeto Appsico, entrega numero 3.

---
+++ Documentação/CRUD/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Appsico</title>
+</head>
+<body>
+    <div class="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4">  
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="login.php" method="POST">
+                                <div>
+                                    <div class="mb-3">
+                                        <label>Usuário</label>
+                                        <input type="text" name="usuario" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div class="mb-3">
+                                    <label>Senha</label>
+                                        <input type="password" name="senha" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div class="mb-3">
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
